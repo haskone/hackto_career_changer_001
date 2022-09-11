ab60c4ef293fa32d404b5cdf8c404e8e07d83fda
Taras Bidiuk <taras@parsehub.com>
2022-09-11 11:45:53 -0400

    fix book a mentor styles

--- src/components/OpenPositions/AvailableMentors/AvailableMentors.js
+++ src/components/OpenPositions/AvailableMentors/AvailableMentors.js
@@ -70,45 +70,47 @@ export default function AvailableMentors({
         })}
       </List>
     </div>
-    <Button
-      variant="contained"
-      color="primary"
-      className={classes.bookAMentor}
-      onClick={() => setOpenMentorDialog(true)}
-    >
-      Book a mentor
-    </Button>
-    <div className={classes.listRoot}>
-      <Typography>Booked Mentors Sessions</Typography>
-      <List dense>
-        {availableMentors.filter((m, index) => mentorsBooked.includes(index)).map((mentor, index) => {
-          const labelId = `checkbox-list-secondary-label-${index}`;
+    <div>
+      <Button
+        variant="contained"
+        color="primary"
+        className={classes.bookAMentor}
+        onClick={() => setOpenMentorDialog(true)}
+      >
+        Book a mentor
+      </Button>
+      <div className={classes.listRoot}>
+        <Typography>Booked Mentors Sessions</Typography>
+        <List dense>
+          {availableMentors.filter((m, index) => mentorsBooked.includes(index)).map((mentor, index) => {
+            const labelId = `checkbox-list-secondary-label-${index}`;
 
-          return (
-            <ListItem
-              selected={selectedMentor === index}
-              key={index}
-              button
-              onClick={() => onSelectMentor(index)}
-            >
-              <ListItemAvatar>
-                <Avatar
-                  alt={`Avatar n°${index + 1}`}
-                  src={
-                    index % 2 ?
-                      "https://cdn-icons-png.flaticon.com/512/2922/2922510.png" :
-                      "https://cdn-icons-png.flaticon.com/512/2922/2922561.png"
-                  }
-                />
-              </ListItemAvatar>
-              <Box>
-                <ListItemText id={labelId} primary={mentor} />
-                <ListItemText id={labelId} primary={timeSlot} />
-              </Box>
-            </ListItem>
-          );
-        })}
-      </List>
+            return (
+              <ListItem
+                selected={selectedMentor === index}
+                key={index}
+                button
+                onClick={() => onSelectMentor(index)}
+              >
+                <ListItemAvatar>
+                  <Avatar
+                    alt={`Avatar n°${index + 1}`}
+                    src={
+                      index % 2 ?
+                        "https://cdn-icons-png.flaticon.com/512/2922/2922510.png" :
+                        "https://cdn-icons-png.flaticon.com/512/2922/2922561.png"
+                    }
+                  />
+                </ListItemAvatar>
+                <Box>
+                  <ListItemText id={labelId} primary={mentor} />
+                  <ListItemText id={labelId} primary={timeSlot} />
+                </Box>
+              </ListItem>
+            );
+          })}
+        </List>
+      </div>
     </div>
     <BookMentorDialog
       open={openMentorDialog}
